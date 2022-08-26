90ea4c86ee28834637667995ec79213e6b017faa
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-26 14:03:39 +0300

    commit tree c6cf862d4cad424da0075c9b986e69b69cc0f9a8

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,7 @@
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[6.1]
+    def change
+        change_column(:students,:birthdate,:datetime)  #This is the actual method that abstracts the change of the db using Sql
+    end
+end
+
+#be soon keen on the spellings for the methods and everything.They messed me.
